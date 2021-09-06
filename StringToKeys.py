8d607f467aa2f1fe916d8d870877ce7427fd7751
Gustavo H <gusthenrsil@gmail.com>
2021-09-06 19:18:39 -0300

    Adicionada a função que retira o último caractere de cada palavra

--- StringToKeys.py
+++ StringToKeys.py
@@ -1,9 +1,25 @@
+def initial_key(words):
+    key_list = []
+    for i in words:
+        character = i[0]
+        key_list.append(character)
+    key = "".join(key_list)
+    return key
+
+def last_key(words):
+    end_characters = []
+    for i in words:
+            position = len(i)-1
+            end_characters.append(i[position])
+    end_key = "".join(end_characters)
+    first_key = initial_key(words)
+    return first_key + end_key
+
+
 string_user = input("Digite sua frase: ")
 words = string_user.split()
-numbers_words = len(words)
-key_list = []
-for i in words:
-    character = i[0]
-    key_list.append(character)
-key = "".join(key_list)
+if len(words) >= 8:
+    key = initial_key(words)
+else:
+    key = last_key(words)
 print(key)
